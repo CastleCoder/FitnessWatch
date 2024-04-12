228c366311d5661e0761ed35a24de90356eb83a2
CastleCoder <cyrillechateau@icloud.com>
2024-04-12 18:24:47 +0200

    Initial Commit

---
+++ FitnessWatch Watch App/FitnessWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessWatchApp.swift
+//  FitnessWatch Watch App
+//
+//  Created by Cyrille Chateau on 12/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct FitnessWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitnessWatch/FitnessWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessWatchApp.swift
+//  FitnessWatch
+//
+//  Created by Cyrille Chateau on 12/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct FitnessWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
